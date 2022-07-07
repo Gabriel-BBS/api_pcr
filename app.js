9bcf76fe5bed6bbb1691d5ecf7127c60058c769b
gabrielb <gabriel.silva900@gmail.com>
2022-07-06 23:06:50 -0300

    Install icon bootstrap, finalização CRUD

--- app.js
+++ app.js
@@ -25,15 +25,37 @@ app.set('view engine','handlebars');
 //arquivo index
 app.get("/",function(req,res){
     res.render('index');
-    // console.log(req.params.id);
 });
 //arquivo inserir
 app.get("/inserir",function(req,res){res.render('inserir');});
 app.post("/controllerForm",urlencodeParser,function(req,res){
-    sql.query("insert into pessoas values(?,?)",[req.body.id,req.body.nome]);
+    sql.query("INSERT INTO pessoas VALUES(?,?)",[req.body.id,req.body.nome]);
     res.render("controllerForm",{nome:req.body.nome});
-})
-
+});
+//arquivo select
+app.get("/select/:id?",function(req,res){
+    if(!req.params.id){
+        sql.query("SELECT * FROM pessoas",function(err,results,fields){
+            res.render('select',{data:results});
+        });   
+    }
+});
+//arquivo deletar
+app.get("/deletar/:id",function(req,res){
+    sql.query("DELETE FROM pessoas WHERE id=?",[req.params.id]);    
+    res.render('deletar');
+});
+//arquivo update
+app.get("/update/:id",function(req,res){
+    sql.query("SELECT * FROM pessoas WHERE id=?",[req.params.id],function(err,results,fields){
+        res.render('update',{id:req.params.id,nome:results[0].nome});
+    });
+    
+});
+app.post("/controllerUpdate",urlencodeParser,function(req,res){
+    sql.query("UPDATE pessoas SET nome=? WHERE id=?",[req.body.nome,req.body.id]);
+    res.render('controllerUpdate');
+});
 //link para arquivo css,js e img
 app.use('/css',express.static('css'));
 app.use('/js',express.static('js'));
