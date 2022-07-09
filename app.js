78e60134c303489a8a058ad7943452a8480101ea
gabrielb <gabriel.silva900@gmail.com>
2022-07-09 08:10:36 -0300

    teste deploy

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const app = express();
 const urlencodeParser = bodyParser.urlencoded({extended:false});
 
 //conecção com o banco de dados MYSQL
-const sql = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'DSAcurso',
-    port:3306
+const sql = mysql.createPool({
+    host:'us-cdbr-east-06.cleardb.net',
+    user:'b83967e24f0613',
+    password:'5ed36278',
+    database:" heroku_edb8033e0c9551b?reconnect=true"
 });
-sql.query("use api_pcr");
+let port = process.env.PORT || 3000;
 
 //template engine
 app.engine('handlebars',handlebars.engine({defaultLayout:'main'}));
@@ -24,44 +24,57 @@ app.set('view engine','handlebars');
 //Routes and templates
 //arquivo index
 app.get("/",function(req,res){
-    res.render('index');
+    res.send('oi');
 });
-//arquivo inserir
-app.get("/inserir",function(req,res){res.render('inserir');});
-app.post("/controllerForm",urlencodeParser,function(req,res){
-    sql.query("INSERT INTO pessoas VALUES(?,?)",[req.body.id,req.body.nome]);
-    res.render("controllerForm",{nome:req.body.nome});
-});
-//arquivo select
-app.get("/select/:id?",function(req,res){
-    if(!req.params.id){
-        sql.query("SELECT * FROM pessoas",function(err,results,fields){
-            res.render('select',{data:results});
-        });   
-    }
-});
-//arquivo deletar
-app.get("/deletar/:id",function(req,res){
-    sql.query("DELETE FROM pessoas WHERE id=?",[req.params.id]);    
-    res.render('deletar');
-});
-//arquivo update
-app.get("/update/:id",function(req,res){
-    sql.query("SELECT * FROM pessoas WHERE id=?",[req.params.id],function(err,results,fields){
-        res.render('update',{id:req.params.id,nome:results[0].nome});
-    });
-    
-});
-app.post("/controllerUpdate",urlencodeParser,function(req,res){
-    sql.query("UPDATE pessoas SET nome=? WHERE id=?",[req.body.nome,req.body.id]);
-    res.render('controllerUpdate');
-});
-//link para arquivo css,js e img
-app.use('/css',express.static('css'));
-app.use('/js',express.static('js'));
-app.use('/img',express.static('img'));
+// //arquivo inserir
+// app.get("/inserir",function(req,res){res.render('inserir');});
+// app.post("/controllerForm",urlencodeParser,function(req,res){
+//     sql.getConnection(function(err,connection){
+//         connection.query("INSERT INTO pessoas VALUES(?,?)",[req.body.id,req.body.nome]);
+//         res.render("controllerForm",{nome:req.body.nome});
+//     });
+ 
+// });
+// //arquivo select
+// app.get("/select/:id?",function(req,res){
+//     if(!req.params.id){
+//         sql.getConnection(function(err,connection){
+//             connection.query("SELECT * FROM pessoas",function(err,results,fields){
+//                 res.render('select',{data:results});
+//             });
+//         });
+         
+//     }
+// });
+// //arquivo deletar
+// app.get("/deletar/:id",function(req,res){
+//     sql.getConnection(function(err,connection){
+//         connection.query("DELETE FROM pessoas WHERE id=?",[req.params.id]);    
+//         res.render('deletar');
+//     })
+// });
+// //arquivo update
+// app.get("/update/:id",function(req,res){
+//     sql.getConnection(function(err,connection){
+//         connection.query("SELECT * FROM pessoas WHERE id=?",[req.params.id],function(err,results,fields){
+//             res.render('update',{id:req.params.id,nome:results[0].nome});
+//         });
+//     });
+     
+// });
+// app.post("/controllerUpdate",urlencodeParser,function(req,res){
+//     sql.getConnection(function(err,connection){
+//         connection.query("UPDATE pessoas SET nome=? WHERE id=?",[req.body.nome,req.body.id]);
+//         res.render('controllerUpdate');
+//     });  
+// });
+// //link para arquivo css,js e img
+// app.use('/css',express.static('css'));
+// app.use('/js',express.static('js'));
+// app.use('/img',express.static('img'));
+// app.use('/node_modules/bootstrap-icons/font/;',express.static('css'));
 
-//Start server
-app.listen(3000,function(req,res){
-    console.log('Servidor está rodando');
-});
+// //Start server
+// app.listen(port,function(req,res){
+//     console.log('Servidor está rodando');
+// });
