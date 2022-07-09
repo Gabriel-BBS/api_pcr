7c390f502d109a7b60b684835d1490e7cd71fa73
gabrielb <gabriel.silva900@gmail.com>
2022-07-09 14:34:10 -0300

    outra tentativa

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const urlencodeParser = bodyParser.urlencoded({extended:false});
 
 //conecção com o banco de dados MYSQL
-const sql = mysql.createPoolCluster({
+const sql = mysql.createPool({
     host:'us-cdbr-east-06.cleardb.net',
     user:'b83967e24f0613',
     password:'5ed36278',
@@ -25,67 +25,65 @@ app.set('view engine','handlebars');
 //arquivo index
 app.get("/",function(req,res){
     res.render('index');
-});
-//arquivo inserir
-app.get("/inserir",function(req,res){res.render('inserir');});
-app.post("/controllerForm",urlencodeParser,function(req,res){
-    sql.getConnection(function(err,connection){
-        connection.query("INSERT INTO pessoas VALUES(?,?)",[req.body.id,req.body.nome]);
-        res.render("controllerForm",{nome:req.body.nome});
-    });
-    if(err){
-        res.render(err);
-    }
-});
+// });
+// //arquivo inserir
+// app.get("/inserir",function(req,res){res.render('inserir');});
+// app.post("/controllerForm",urlencodeParser,function(req,res){
+//     sql.getConnection(function(err,connection){
+//         connection.query("INSERT INTO pessoas VALUES(?,?)",[req.body.id,req.body.nome]);
+//         res.render("controllerForm",{nome:req.body.nome});
+//     });
+//     if(err){
+//         res.render(err);
+//     }
+// });
 //arquivo select
 app.get("/select/:id?",function(req,res){
     if(!req.params.id){
         sql.getConnection(function(err,connection){
             connection.query("SELECT * FROM pessoas",function(err,results,fields){
                 res.render('select',{data:results});
+            if(err) throw err;
             });
-            if(err){
-                res.render(err);
-            }
         });
          
-    }else{
-        sql.getConnection(function(err,connection){
-            sql.query("select * from pessoas where id=?",[req.params.id],function(err,results,fields){
-                res.render('select',{data:results});
-            });
-        if(err){
-            res.render(err);
-        }
-        });
-    }
+    }//else{
+    //     sql.getConnection(function(err,connection){
+    //         sql.query("select * from pessoas where id=?",[req.params.id],function(err,results,fields){
+    //             res.render('select',{data:results});
+    //         });
+    //     if(err){
+    //         res.render(err);
+    //     }
+    //     });
+    // }
 }); 
-//arquivo deletar
-app.get("/deletar/:id",function(req,res){
-    sql.getConnection(function(err,connection){
-        connection.query("DELETE FROM pessoas WHERE id=?",[req.params.id]);    
-        res.render('deletar');
+// //arquivo deletar
+// app.get("/deletar/:id",function(req,res){
+//     sql.getConnection(function(err,connection){
+//         connection.query("DELETE FROM pessoas WHERE id=?",[req.params.id]);    
+//         res.render('deletar');
        
-    });
-    if(err){
-        res.render(err);
-    }
-});
-//arquivo update
-app.get("/update/:id",function(req,res){
-    sql.getConnection(function(err,connection){
-        connection.query("SELECT * FROM pessoas WHERE id=?",[req.params.id],function(err,results,fields){
-            res.render('update',{id:req.params.id,nome:results[0].nome});
-        });
-    });
+//     });
+//     if(err){
+//         res.render(err);
+//     }
+// });
+// //arquivo update
+// app.get("/update/:id",function(req,res){
+//     sql.getConnection(function(err,connection){
+//         connection.query("SELECT * FROM pessoas WHERE id=?",[req.params.id],function(err,results,fields){
+//             res.render('update',{id:req.params.id,nome:results[0].nome});
+//         });
+//     });
      
-});
-app.post("/controllerUpdate",urlencodeParser,function(req,res){
-    sql.getConnection(function(err,connection){
-        connection.query("UPDATE pessoas SET nome=? WHERE id=?",[req.body.nome,req.body.id]);
-        res.render('controllerUpdate');
-    });  
-});
+// });
+// app.post("/controllerUpdate",urlencodeParser,function(req,res){
+//     sql.getConnection(function(err,connection){
+//         connection.query("UPDATE pessoas SET nome=? WHERE id=?",[req.body.nome,req.body.id]);
+//         res.render('controllerUpdate');
+//     });  
+// });
 //link para arquivo css,js e img
 app.use('/css',express.static('css'));
 app.use('/js',express.static('js'));
