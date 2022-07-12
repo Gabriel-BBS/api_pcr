5c4b36c1f5e198a7d97dcd7284cca1c37ee7dc31
gabrielb <gabriel.silva900@gmail.com>
2022-07-11 23:07:02 -0300

    teste CRUD funcionando

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const sql = mysql.createPool({
     host:'us-cdbr-east-06.cleardb.net',
     user:'b83967e24f0613',
     password:'5ed36278',
-    database:" heroku_edb8033e0c9551b?reconnect=true"
+    database:"heroku_edb8033e0c9551b"
 });
 let port = process.env.PORT || 3000;
 
@@ -24,50 +24,55 @@ app.set('view engine','handlebars');
 //Routes and templates
 //arquivo index
 app.get("/",function(req,res){
-    res.send('oi');
+    res.send('index');
 });
 // //arquivo inserir
-// app.get("/inserir",function(req,res){res.render('inserir');});
-// app.post("/controllerForm",urlencodeParser,function(req,res){
-//     sql.getConnection(function(err,connection){
-//         connection.query("INSERT INTO pessoas VALUES(?,?)",[req.body.id,req.body.nome]);
-//         res.render("controllerForm",{nome:req.body.nome});
-//     });
+app.get("/inserir",function(req,res){res.render('inserir');});
+app.post("/controllerForm",urlencodeParser,function(req,res){
+    sql.getConnection(function(err,connection){
+        connection.query("INSERT INTO pessoas VALUES(?,?)",[req.body.id,req.body.nome]);
+        connection.release();
+        res.render("controllerForm",{nome:req.body.nome});
+    });
  
-// });
+});
 // //arquivo select
-// app.get("/select/:id?",function(req,res){
-//     if(!req.params.id){
-//         sql.getConnection(function(err,connection){
-//             connection.query("SELECT * FROM pessoas",function(err,results,fields){
-//                 res.render('select',{data:results});
-//             });
-//         });
+app.get("/select/:id?",function(req,res){
+    if(!req.params.id){
+        sql.getConnection(function(err,connection){
+            connection.query("SELECT * FROM pessoas",function(err,results,fields){
+                connection.release();
+                res.render('select',{data:results});
+            });
+        });
          
-//     }
-// });
+    }
+});
 // //arquivo deletar
-// app.get("/deletar/:id",function(req,res){
-//     sql.getConnection(function(err,connection){
-//         connection.query("DELETE FROM pessoas WHERE id=?",[req.params.id]);    
-//         res.render('deletar');
-//     })
-// });
+app.get("/deletar/:id",function(req,res){
+    sql.getConnection(function(err,connection){
+        connection.query("DELETE FROM pessoas WHERE id=?",[req.params.id]);
+        connection.release();    
+        res.render('deletar');
+    })
+});
 // //arquivo update
-// app.get("/update/:id",function(req,res){
-//     sql.getConnection(function(err,connection){
-//         connection.query("SELECT * FROM pessoas WHERE id=?",[req.params.id],function(err,results,fields){
-//             res.render('update',{id:req.params.id,nome:results[0].nome});
-//         });
-//     });
+app.get("/update/:id",function(req,res){
+    sql.getConnection(function(err,connection){
+        connection.query("SELECT * FROM pessoas WHERE id=?",[req.params.id],function(err,results,fields){
+            connection.release();
+            res.render('update',{id:req.params.id,nome:results[0].nome});
+        });
+    });
      
-// });
-// app.post("/controllerUpdate",urlencodeParser,function(req,res){
-//     sql.getConnection(function(err,connection){
-//         connection.query("UPDATE pessoas SET nome=? WHERE id=?",[req.body.nome,req.body.id]);
-//         res.render('controllerUpdate');
-//     });  
-// });
+});
+app.post("/controllerUpdate",urlencodeParser,function(req,res){
+    sql.getConnection(function(err,connection){
+        connection.query("UPDATE pessoas SET nome=? WHERE id=?",[req.body.nome,req.body.id]);
+        connection.release();
+        res.render('controllerUpdate');
+    });  
+});
 //link para arquivo css,js e img
 app.use('/css',express.static('css'));
 app.use('/js',express.static('js'));
